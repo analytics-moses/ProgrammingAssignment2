6ab76fe09f542feb8a131be74064abefcc03dc3d
analytics-moses <45475150+analytics-moses@users.noreply.github.com>
2019-01-02 20:26:13 +1100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -28,21 +28,3 @@ cachemean <- function(x, ...) {
         x$setmean(m)
         m
 }
-#=======
-## changed 01-Jan-19
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-#>>>>>>> ba40244090017f0fc23b55ec72ebd6be08bc6708
